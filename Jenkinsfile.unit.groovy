45ceb88507b760ebbc8777542b4d44bd3ef4dd25
DaveWS <davidtxus@hotmail.com>
2022-06-20 09:50:42 -0500

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -15,34 +15,22 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('Api tests') {
+        stage('Integration tests'){
             steps {
                 sh 'make test-api'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('e2e tests') {
+        stage('E2E tests'){
             steps {
                 sh 'make test-e2e'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-       
     }
     post {
         always {
             junit 'results/*_result.xml'
-            cleanWs()
         }
-        success {  
-             echo "El nombre del trabajo es: ${JOB_NAME}"
-             echo "El numero de ejecucion es: ${BUILD_NUMBER}"
-             
-         }  
-        
-       failure {  
-             mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "foo@foomail.com";  
-         }   
-        
     }
 }
